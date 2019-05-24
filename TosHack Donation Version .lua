6e43768cc27b8d75d03a6edad0713225f8080e28
hsuweien <46742852+hsuweien@users.noreply.github.com>
2019-05-25 03:54:46 +0800

    Add files via upload

--- TosHack Donation Version .lua
+++ TosHack Donation Version .lua
@@ -1,16 +1,16 @@
 function writefile()
-JT = math.random(459867,2694599)--從459867到2694599範圍中隨機生一個(機型碼=(暫存檔))
-file=io.open("/storage/emulated/0/Android/.system_int",'r')--檢測是否有暫存檔
+JT = math.random(459867,2694599)
+file=io.open("/storage/emulated/0/Android/.system_int",'r')
 if file == nil then
-file=io.open("/storage/emulated/0/Android/.system_int",'w')--若沒有則生成一個暫存檔
+file=io.open("/storage/emulated/0/Android/.system_int",'w')
 file:write(JT)
 
-JTfw=io.open("/storage/emulated/0/Android/.system_int",'r')--檢測暫存檔內容
+JTfw=io.open("/storage/emulated/0/Android/.system_int",'r')
  JTdia = JTfw:read('*a')
   JTfw:close()
   
 else
-JTfw=io.open("/storage/emulated/0/Android/.system_int",'r')--二次檢查暫存檔內容
+JTfw=io.open("/storage/emulated/0/Android/.system_int",'r')
  JTdia = JTfw:read('*a')
   JTfw:close()
 
@@ -28,22 +28,21 @@ function check()
          if JTmu == nil then
    os.exit()
      end
-  if JTmu[1] == nil then gg.alert("沒輸入密碼哦~~") os.exit() end --沒輸入密碼提示語句
+  if JTmu[1] == nil then gg.alert("沒輸入密碼哦~~") os.exit() end 
 
-a1 = (JTmu[1]+154546596)/JTmu[2]--算法
+a1 = (JTmu[1]+154546596)/JTmu[2]
 JT = a1..""
 
-if JT == 365787 then gg.alert("密碼正確哦!") --密碼正確提示語句
+if JT == 365787 then gg.alert("密碼正確哦!") 
 else
-gg.alert("密碼錯誤") os.exit()--密碼錯誤提示語句
+gg.alert("密碼錯誤") os.exit()
 end
 end
-JTs = gg.alert("俊廷測試機型碼系統","開始測試")
+JTs = gg.alert("愛心捐款版本登入系統","點我登入")
 if JTs == 1 then writefile() else
 os.exit() 
 end
---↓↓源腳本↓↓
-	
+
 function donationhack ()
     local funcTable= gg.multiChoice({'①秒怪','②究極秒關'}, nil, '請選擇愛心捐款數據功能')
 		if not funcTable then
