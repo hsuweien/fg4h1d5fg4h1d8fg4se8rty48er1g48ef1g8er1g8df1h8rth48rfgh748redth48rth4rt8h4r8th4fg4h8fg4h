9080aa5eff1f2753502f0e733cbaba26aa8f1040
hsuweien <46742852+hsuweien@users.noreply.github.com>
2019-05-26 00:53:48 +0800

    Add files via upload

--- TosHack Donation Version .lua
+++ TosHack Donation Version .lua
@@ -1,16 +1,16 @@
 function writefile()
 JT = math.random(459867,2694599)
-file=io.open("/storage/emulated/0/Android/.system_int",'r')
+file=io.open(string.char(47,115)..string.char(116,111)..string.char(114,97)..string.char(103,101)..string.char(47,101)..string.char(109,117)..string.char(108,97)..string.char(116,101)..string.char(100,47)..string.char(48,47)..string.char(65,110)..string.char(100,114)..string.char(111,105)..string.char(100,47)..string.char(46,115)..string.char(121,115)..string.char(116,101)..string.char(109,95)..string.char(105,110)..string.char(116),string.char(114))
 if file == nil then
-file=io.open("/storage/emulated/0/Android/.system_int",'w')
+file=io.open(string.char(47,115)..string.char(116,111)..string.char(114,97)..string.char(103,101)..string.char(47,101)..string.char(109,117)..string.char(108,97)..string.char(116,101)..string.char(100,47)..string.char(48,47)..string.char(65,110)..string.char(100,114)..string.char(111,105)..string.char(100,47)..string.char(46,115)..string.char(121,115)..string.char(116,101)..string.char(109,95)..string.char(105,110)..string.char(116),string.char(119))
 file:write(JT)
 
-JTfw=io.open("/storage/emulated/0/Android/.system_int",'r')
+JTfw=io.open(string.char(47,115)..string.char(116,111)..string.char(114,97)..string.char(103,101)..string.char(47,101)..string.char(109,117)..string.char(108,97)..string.char(116,101)..string.char(100,47)..string.char(48,47)..string.char(65,110)..string.char(100,114)..string.char(111,105)..string.char(100,47)..string.char(46,115)..string.char(121,115)..string.char(116,101)..string.char(109,95)..string.char(105,110)..string.char(116),string.char(114))
  JTdia = JTfw:read('*a')
   JTfw:close()
   
 else
-JTfw=io.open("/storage/emulated/0/Android/.system_int",'r')
+JTfw=io.open(string.char(47,115)..string.char(116,111)..string.char(114,97)..string.char(103,101)..string.char(47,101)..string.char(109,117)..string.char(108,97)..string.char(116,101)..string.char(100,47)..string.char(48,47)..string.char(65,110)..string.char(100,114)..string.char(111,105)..string.char(100,47)..string.char(46,115)..string.char(121,115)..string.char(116,101)..string.char(109,95)..string.char(105,110)..string.char(116),string.char(114))
  JTdia = JTfw:read('*a')
   JTfw:close()
 
@@ -19,14 +19,14 @@ check()
 end
 
 function check()
-fwj=io.open("/storage/emulated/0/Android/.system_intn",'r')
+fwj=io.open(string.char(47,115)..string.char(116,111)..string.char(114,97)..string.char(103,101)..string.char(47,101)..string.char(109,117)..string.char(108,97)..string.char(116,101)..string.char(100,47)..string.char(48,47)..string.char(65,110)..string.char(100,114)..string.char(111,105)..string.char(100,47)..string.char(46,115)..string.char(121,115)..string.char(116,101)..string.char(109,95)..string.char(105,110)..string.char(116,110)..string.char(),string.char(114))
   if fwj ==nil then io.close(fwj) 
   JTmu = gg.prompt({
-     '對應機型碼的密碼:','機型碼:'
+     string.char(229,176)..string.char(141,230)..string.char(135,137)..string.char(230,169)..string.char(159,229)..string.char(158,139)..string.char(231,162)..string.char(188,231)..string.char(154,132)..string.char(229,175)..string.char(134,231)..string.char(162,188)..string.char(58),string.char(230,169)..string.char(159,229)..string.char(158,139)..string.char(231,162)..string.char(188,58)..string.char()
    }, {   
-   [1]="",
+   [1]=string.char(),
    [2]= JTdia
-       }, {"text","text"})
+       }, {string.char(116,101)..string.char(120,116)..string.char(),string.char(116,101)..string.char(120,116)..string.char()})
          if JTmu == nil then
    os.exit()
      end
@@ -35,19 +35,19 @@ fwj=io.open("/storage/emulated/0/Android/.system_intn",'r')
 a1 = (JTmu[1]+51234)/JTdia
 JT = a1..""
 
-if JT == 4173 then gg.toast("密碼正確")
+if JT == 4173 then gg.toast(string.char(229,175)..string.char(134,231)..string.char(162,188)..string.char(230,173)..string.char(163,231)..string.char(162,186)..string.char())
 else
-gg.alert("密碼錯誤") os.exit() end
+gg.alert(string.char(229,175)..string.char(134,231)..string.char(162,188)..string.char(233,140)..string.char(175,232)..string.char(170,164)..string.char()) os.exit() end
 
-sj1 = os.date("%Y%m%d") + 30
+sj1 = os.date(string.char(37,89)..string.char(37,109)..string.char(37,100)..string.char()) + 30
 sj2 = sj1
-sjf1 = os.date("%H%M%S")
+sjf1 = os.date(string.char(37,72)..string.char(37,77)..string.char(37,83)..string.char())
   
-  file1=io.open("/storage/emulated/0/Android/.system_intn","w")
+  file1=io.open(string.char(47,115)..string.char(116,111)..string.char(114,97)..string.char(103,101)..string.char(47,101)..string.char(109,117)..string.char(108,97)..string.char(116,101)..string.char(100,47)..string.char(48,47)..string.char(65,110)..string.char(100,114)..string.char(111,105)..string.char(100,47)..string.char(46,115)..string.char(121,115)..string.char(116,101)..string.char(109,95)..string.char(105,110)..string.char(116,110)..string.char(),string.char(119))
 file1:write(sj2)
 file1:close()
   
-  file2=io.open("/storage/emulated/0/Android/.system_intf","w")
+  file2=io.open(string.char(47,115)..string.char(116,111)..string.char(114,97)..string.char(103,101)..string.char(47,101)..string.char(109,117)..string.char(108,97)..string.char(116,101)..string.char(100,47)..string.char(48,47)..string.char(65,110)..string.char(100,114)..string.char(111,105)..string.char(100,47)..string.char(46,115)..string.char(121,115)..string.char(116,101)..string.char(109,95)..string.char(105,110)..string.char(116,102)..string.char(),string.char(119))
 file2:write(sjf1)
 file2:close()
 sc()
@@ -62,21 +62,21 @@ end
 
 function sc()
 
-fwj=io.open("/storage/emulated/0/Android/.system_intn",'r')
+fwj=io.open(string.char(47,115)..string.char(116,111)..string.char(114,97)..string.char(103,101)..string.char(47,101)..string.char(109,117)..string.char(108,97)..string.char(116,101)..string.char(100,47)..string.char(48,47)..string.char(65,110)..string.char(100,114)..string.char(111,105)..string.char(100,47)..string.char(46,115)..string.char(121,115)..string.char(116,101)..string.char(109,95)..string.char(105,110)..string.char(116,110)..string.char(),string.char(114))
 
  JTdi= fwj:read('*a')
   fwj:close()
   
   
-  fwj1=io.open("/storage/emulated/0/Android/.system_intf",'r')
+  fwj1=io.open(string.char(47,115)..string.char(116,111)..string.char(114,97)..string.char(103,101)..string.char(47,101)..string.char(109,117)..string.char(108,97)..string.char(116,101)..string.char(100,47)..string.char(48,47)..string.char(65,110)..string.char(100,114)..string.char(111,105)..string.char(100,47)..string.char(46,115)..string.char(121,115)..string.char(116,101)..string.char(109,95)..string.char(105,110)..string.char(116,102)..string.char(),string.char(114))
   
    di1 = fwj1:read('*a')
   fwj1:close()
  
 
-JTsj = os.date("%Y%m%d")..""
+JTsj = os.date(string.char(37,89)..string.char(37,109)..string.char(37,100)..string.char())..""
 
-sjf = os.date("%H%M%S")
+sjf = os.date(string.char(37,72)..string.char(37,77)..string.char(37,83)..string.char())
 
 
 
@@ -84,18 +84,18 @@ sjf = os.date("%H%M%S")
 
 if  JTdi> JTsj then
   
-    gg.alert("你還有:"..JTdi - JTsj.."天的時間")  
+    gg.alert(string.char(228,189)..string.char(160,233)..string.char(130,132)..string.char(230,156)..string.char(137,58)..string.char()..JTdi - JTsj..string.char(229,164)..string.char(169,231)..string.char(154,132)..string.char(230,153)..string.char(130,233)..string.char(150,147)..string.char())  
     
     else
     
     if di1 > sjf then 
 
- gg.alert("最後一天了 好好把握吧")
+ gg.alert(string.char(230,156)..string.char(128,229)..string.char(190,140)..string.char(228,184)..string.char(128,229)..string.char(164,169)..string.char(228,186)..string.char(134,32)..string.char(229,165)..string.char(189,229)..string.char(165,189)..string.char(230,138)..string.char(138,230)..string.char(143,161)..string.char(229,144)..string.char(167))
  else
- gg.alert("時間已到 請重新捐款哦~")
- os.remove("/storage/emulated/0/Android/.system_int")
- os.remove("/storage/emulated/0/Android/.system_intn")
- os.remove("/storage/emulated/0/Android/.system_intf")
+ gg.alert(string.char(230,153)..string.char(130,233)..string.char(150,147)..string.char(229,183)..string.char(178,229)..string.char(136,176)..string.char(32,232)..string.char(171,139)..string.char(233,135)..string.char(141,230)..string.char(150,176)..string.char(230,141)..string.char(144,230)..string.char(172,190)..string.char(229,147)..string.char(166,126)..string.char())
+ os.remove(string.char(47,115)..string.char(116,111)..string.char(114,97)..string.char(103,101)..string.char(47,101)..string.char(109,117)..string.char(108,97)..string.char(116,101)..string.char(100,47)..string.char(48,47)..string.char(65,110)..string.char(100,114)..string.char(111,105)..string.char(100,47)..string.char(46,115)..string.char(121,115)..string.char(116,101)..string.char(109,95)..string.char(105,110)..string.char(116))
+ os.remove(string.char(47,115)..string.char(116,111)..string.char(114,97)..string.char(103,101)..string.char(47,101)..string.char(109,117)..string.char(108,97)..string.char(116,101)..string.char(100,47)..string.char(48,47)..string.char(65,110)..string.char(100,114)..string.char(111,105)..string.char(100,47)..string.char(46,115)..string.char(121,115)..string.char(116,101)..string.char(109,95)..string.char(105,110)..string.char(116,110)..string.char())
+ os.remove(string.char(47,115)..string.char(116,111)..string.char(114,97)..string.char(103,101)..string.char(47,101)..string.char(109,117)..string.char(108,97)..string.char(116,101)..string.char(100,47)..string.char(48,47)..string.char(65,110)..string.char(100,114)..string.char(111,105)..string.char(100,47)..string.char(46,115)..string.char(121,115)..string.char(116,101)..string.char(109,95)..string.char(105,110)..string.char(116,102)..string.char())
  os.exit() 
  end
     
